4189f9bdc6d4acb430d013c0ceda7b1fd25ccde6
Gwenaël <g.magnenat@gmail.com>
2021-01-14 20:56:18 +0100

    Add event listeners for the two buttons

--- script.js
+++ script.js
@@ -41,6 +41,16 @@ async function getQuote() {
 	}
 }
 
+// Tweet Quote
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On Load
 getQuote();
 // newQuote();
