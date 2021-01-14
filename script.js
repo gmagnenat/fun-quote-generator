18f6e51918697bf5df39c644e878cdb037c2f58d
Gwenaël <g.magnenat@gmail.com>
2021-01-14 20:28:38 +0100

    Fetch data from the api and get a random single quote

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Get Quote From Api
+let apiQuotes = [];
+
+// Show new Quote
+function newQuote() {
+	// Pick a random quote from apiQuotes array
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+	console.log(quote);
+}
+
+//Get Quote From Api
+async function getQuote() {
+	const apiUrl = 'https://type.fit/api/quotes';
+	try {
+		const response = await fetch(apiUrl);
+		apiQuotes = await response.json();
+		newQuote();
+	} catch (error) {
+		// Catch Error here
+		console.error('whoops, no quote', error);
+	}
+}
+
+// On Load
+getQuote();
+// newQuote();
