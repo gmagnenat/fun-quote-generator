9a4c4c25552166410d82be12569bca788db6e411
Gwenaël <g.magnenat@gmail.com>
2021-01-14 20:40:11 +0100

    Place data in the html for quote and quote author

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show new Quote
 function newQuote() {
 	// Pick a random quote from apiQuotes array
 	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-	console.log(quote);
+
+	// Check if Author field is blank and replace it with 'Unknown'
+	if (!quote.author) {
+		authorText.textContent = 'Unknown';
+	} else {
+		authorText.textContent = quote.author;
+	}
+
+	// Check quote length to change styling
+	if (quote.text.length > 50) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+
+	quoteText.textContent = quote.text;
 }
 
 //Get Quote From Api
