a91df4c99f9b9f3342f7d08dc27f64753e9645e0
Gwenaël <g.magnenat@gmail.com>
2021-01-15 14:43:39 +0100

    Switch back to the api with the adition of a proxy hosted on heroku

--- script.js
+++ script.js
@@ -3,41 +3,50 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
-// Show new Quote
-function newQuote() {
-	// Pick a random quote from apiQuotes array
-	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+// Show loading
+function showLoadingSpinner() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
 
-	// Check if Author field is blank and replace it with 'Unknown'
-	if (!quote.author) {
-		authorText.textContent = 'Unknown';
-	} else {
-		authorText.textContent = quote.author;
-	}
-
-	// Check quote length to change styling
-	if (quote.text.length > 50) {
-		quoteText.classList.add('long-quote');
-	} else {
-		quoteText.classList.remove('long-quote');
-	}
-
-	quoteText.textContent = quote.text;
+// Hide loading
+function removeLoadingSpinner() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
 }
 
 //Get Quote From Api
 async function getQuote() {
-	const apiUrl = 'https://type.fit/api/quotes';
+	showLoadingSpinner();
+	const proxyUrl = 'https://nameless-inlet-02329.herokuapp.com/';
+	const apiUrl =
+		'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 	try {
-		const response = await fetch(apiUrl);
-		apiQuotes = await response.json();
-		newQuote();
+		const response = await fetch(proxyUrl + apiUrl);
+		const data = await response.json();
+
+		// Handle unkown authors
+		if (data.quoteAuthor === '') {
+			authorText.innerText = 'Unknown';
+		} else {
+			authorText.innerText = data.quoteAuthor;
+		}
+
+		// Reduce font size for long quotes
+		if (data.quoteText.length > 120) {
+			quoteText.classList.add('long-quote');
+		} else {
+			quoteText.classList.remove('long-quote');
+		}
+		quoteText.innerText = data.quoteText;
+		removeLoadingSpinner();
 	} catch (error) {
 		// Catch Error here
-		console.error('whoops, no quote', error);
+		getQuote();
 	}
 }
 
@@ -48,7 +57,7 @@ function tweetQuote() {
 }
 
 // Event Listeners
-newQuoteBtn.addEventListener('click', newQuote);
+newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
